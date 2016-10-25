8e4eb2b9bbf59a550914644d24ce77e7fa605070
Dash612 <kylejulien612@gmail.com>
2016-10-25 13:31:10 -0700

    Update GameBoard.hpp

--- GameBoard.hpp
+++ GameBoard.hpp
@@ -20,10 +20,10 @@ public:
    // Build the gameboard
    // TO DO: implement this function
    void buildBoard();
-   
    GameBoard() {
+	   board.at(0);
+	   buildBoard();
       // TODO: implement this function properly
-      throw std::logic_error("not implemented yet");
    }
    
    // If player lands on chutes or ladders, returns the new position.
@@ -35,10 +35,70 @@ public:
          throw range_error("index out of bounds");
       }
       // TODO: implement this function properly
-      throw std::logic_error("not implemented yet");
+	  if (position == ONE) {
+		  return 38;
+	  }
+	  if (position == FOUR) {
+		  return 14;
+	  }
+	  if (position == NINE) {
+		  return 31;
+	  }
+	  if (position == TWENTY_ONE) {
+		  return 42;
+	  }
+	  if (position == TWENTY_EIGHT) {
+		  return 84;
+	  }
+	  if (position == THIRTY_SIX) {
+		  return 44;
+	  }
+	  if (position == FIFTY_ONE) {
+		  return 67;
+	  }
+	  if (position == SEVENTY_ONE) {
+		  return 91;
+	  }
+	  if (position == EIGHTY) {
+		  return 100;
+	  }
+	  if (position == SIXTEEN) {
+		  return 6;
+	  }
+	  if (position == FORTY_SEVEN) {
+		  return 26;
+	  }
+	  if (position == FORTY_NINE) {
+		  return 11;
+	  }
+	  if (position == FIFTY_SIX) {
+		  return 53;
+	  }
+	  if (position == SIXTY_TWO) {
+		  return 19;
+	  }
+	  if (position == SIXTY_FOUR) {
+		  return 60;
+	  }
+	  if (position == EIGHTY_SEVEN) {
+		  return 24;
+	  }
+	  if (position == NINETY_THREE) {
+		  return 73;
+	  }
+	  if (position == NINETY_FIVE) {
+		  return 75;
+	  }
+	  if (position == NINETY_EIGHT) {
+		  return 78;
+	  }
+	  return position;
    }
    
 private:
+	ExtendableVector<int>board;
+	int playerPosition;
+	int newPosition;
    // TO DO: add storage for squares including square of chutes and ladders
    // Requirement: use ExtendableVector to store the square
 };
